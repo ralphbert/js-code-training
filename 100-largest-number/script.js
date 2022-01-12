69940401673b2e690c8c071d047281492e138ae8
Ralph Harrer <harrer.ralph@gmail.com>
2022-01-12 08:50:28 +0100

    added largest number, added immutable list reversal

---
+++ 100-largest-number/script.js
@@ -0,0 +1,28 @@
+const numbers1 = [200, 10, -5, 7, 31, 17, 22, -312, 100, 88, 0, 3];
+const numbers2 = [-1, -4, null, undefined, 'Hi!', -1000, 0];
+const numbers3 = [];
+const numbers4 = null;
+const numbers5 = undefined;
+
+// ########################################
+// TODO Step 1
+// ########################################
+
+function getMaximum(numbers) {
+    // TODO: implement me by using a for loop
+    return 0;
+}
+
+// TODO: get maximum number for each array (see number1 - numbers5) by using getMaximum() and use console.log to print them
+
+
+// ########################################
+// TODO Step 2
+// ########################################
+
+function getMaximumByReducing(numbers) {
+    // TODO: implement me by using the reduce-function
+    return 0;
+}
+
+// TODO: get maximum number for each array (see number1 - numbers5) by using getMaximumByReducing() and use console.log to print them
