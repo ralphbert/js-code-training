c9858ac975597be2e411f67840fa3d1389c8e0b5
Ralph Harrer <harrer.ralph@gmail.com>
2022-01-13 08:40:29 +0100

    added mutable array reversal

---
+++ 120-list-reversal-mutable/script.js
@@ -0,0 +1,17 @@
+const items1 = [1, 2, 3, 4, 5, 6, 7];
+const items2 = ['Bob', 'Anne', 7, true];
+const items3 = [];
+const items4 = null;
+const items5 = undefined;
+
+function getReverseArray(items) {
+    // TODO: implement me by using a for loop
+    return undefined;
+}
+
+// TODO: call getReverseArray() for each array (see number1 - numbers5) and use console.log to print them
+
+console.log('items1 before', items1);
+console.log('items1 after', getReverseArray(items1));
+
+// TODO to the rest for each array items2, items3, ...
